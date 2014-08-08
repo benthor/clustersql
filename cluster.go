c346cf6b181808dca7b7d2fc2a77b96436e4ff5c
Thorben Krüger <tkr@ecix.net>
2014-08-08 18:36:47 +0200

    major code cleanup and documentation

--- cluster.go
+++ cluster.go
@@ -1,9 +1,84 @@
+// Copyright 2014 by tkr@ecix.net (Peering GmbH)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Go Clustering SQL Driver - A clustering, implementation-agnostic
+// "meta"-driver for any backend implementing "database/sql/driver".
+//
+// It does (latency-based) load-balancing and error-recovery over all registered
+// nodes.
+//
+// It is assumed that database-state is transparently replicated over all
+// nodes by some database-side clustering solution. This driver ONLY handles
+// the client side of such a cluster.
+//
+// All errors which are made non-fatal because of failover are logged.
+//
+// To make use of clustering, use clustersql with any backend driver
+// implementing "database/sql/driver" like so:
+//
+//  import "database/sql"
+//  import "github.com/go-sql-driver/mysql"
+//  import "github.com/benthor/clustersql"
+//
+// There is currently no way around instanciating the backend driver explicitly
+//
+//  mysqlDriver := mysql.MySQLDriver{}
+//
+// You can perform backend-driver specific settings such as
+//
+//  err := mysql.SetLogger(mylogger)
+//
+// Create a new clustering driver with the backend driver
+//
+//	clusterDriver := clustersql.NewDriver(mysqlDriver)
+//
+// Add nodes, including driver-specific name format, in this case Go-MySQL DSN.
+// Here, we add three nodes belonging to a galera (https://mariadb.com/kb/en/mariadb/documentation/replication-cluster-multi-master/galera/) cluster
+//
+//	clusterDriver.AddNode("galera1", "user:password@tcp(dbhost1:3306)/db")
+//	clusterDriver.AddNode("galera2", "user:password@tcp(dbhost2:3306)/db")
+//	clusterDriver.AddNode("galera3", "user:password@tcp(dbhost3:3306)/db")
+//
+// Make the clusterDriver available to the go sql interface under an arbitrary
+// name
+//
+//	sql.Register("myCluster", clusterDriver)
+//
+// Open the registered clusterDriver with an arbitrary DSN string (not used)
+//
+//	db, err := sql.Open("myCluster", "whatever")
+//
+// Continue to use the sql interface as documented at
+// http://golang.org/pkg/database/sql/
 package clustersql
 
 import "database/sql/driver"
 import "log"
 import "time"
 
+// ClusterError is an error type which represents an unrecoverable Error
 type ClusterError struct {
 	Message string
 }
@@ -12,24 +87,34 @@ func (me ClusterError) Error() string {
 	return me.Message
 }
 
+// Cluster is a type which implements "database/sql/driver"
 type Cluster struct {
-	Nodes  map[string]*Node
-	Driver driver.Driver
+	Nodes  map[string]*Node // maps a node's name to its registered instance
+	Driver driver.Driver    // the upstream database driver
 }
 
+// AddNode registeres backend connection information with the driver
+//
+// dataSourceName will get passed to the "Open" call of the backend driver
 func (cluster *Cluster) AddNode(nodeName, dataSourceName string) {
 	cluster.Nodes[nodeName] = &Node{nodeName, dataSourceName, nil, false, nil}
 }
 
+// Node is a type describing one node in the Cluster
 type Node struct {
-	Name           string
-	dataSourceName string
-	Conn           driver.Conn
-	Waiting        bool
-	Err            error
+	Name           string      // node name
+	dataSourceName string      // DSN for the backend driver
+	Conn           driver.Conn // A currently cached backend connection
+	Waiting        bool        // the node is currently waiting for the backend to open a connection
+	Err            error       // the last error that was seen by the node
 }
 
-func (cluster *Cluster) getConn() (driver.Conn, error) {
+// GetConn concurrently opens connections to all nodes in the cluster, returning the first successfully opened driver.Conn.
+//
+// When opening of a connection fails, an automatic retry after 30 seconds (currently hardcoded) is scheduled. This also generates a log entry with error details.
+//
+// If no connections could be opened within 45 seconds (currently hardcoded), a ClusterError is returned.
+func (cluster *Cluster) GetConn() (driver.Conn, error) {
 	nodec := make(chan *Node)
 	die := make(chan bool)
 	for _, node := range cluster.Nodes {
@@ -38,8 +123,16 @@ func (cluster *Cluster) getConn() (driver.Conn, error) {
 			go func(nodec chan *Node, node Node, die chan bool) {
 				cluster.Nodes[node.Name] = &node
 				node.Waiting = true
-				node.Conn, node.Err = cluster.Driver.Open(node.dataSourceName)
-				node.Waiting = false
+				for {
+					node.Conn, node.Err = cluster.Driver.Open(node.dataSourceName)
+					if node.Err != nil {
+						log.Println(node.Name, node.Err)
+						<-time.Tick(30 * time.Second)
+					} else {
+						node.Waiting = false
+						break
+					}
+				}
 				select {
 				case nodec <- &node:
 					// log.Println(node.Name, "connected")
@@ -60,13 +153,16 @@ func (cluster *Cluster) getConn() (driver.Conn, error) {
 	case node := <-nodec:
 		close(die)
 		return node.Conn, node.Err
-	case <-time.After(30 * time.Second):
+	case <-time.After(45 * time.Second):
 		return nil, ClusterError{"Could not open any connection!"}
 	}
 }
 
+// Prepare works as documented at http://golang.org/pkg/database/sql/#DB.Prepare
+//
+// The query is executed on the node that reponds quickest
 func (cluster Cluster) Prepare(query string) (driver.Stmt, error) {
-	conn, err := cluster.getConn()
+	conn, err := cluster.GetConn()
 	if err != nil {
 		return nil, err
 	}
@@ -74,6 +170,9 @@ func (cluster Cluster) Prepare(query string) (driver.Stmt, error) {
 	return conn.Prepare(query)
 }
 
+// Close works on all backend-connections that are the clusterDriver has cached
+//
+// Always returns nil for now, errors are merely logged
 func (cluster Cluster) Close() error {
 	for name, node := range cluster.Nodes {
 		if node.Conn != nil {
@@ -86,26 +185,24 @@ func (cluster Cluster) Close() error {
 	return nil
 }
 
+// Begin works as documented at http://golang.org/pkg/database/sql/#DB.Begin
+//
+// Begin() is called on the backend connection that is available quickest
 func (cluster Cluster) Begin() (driver.Tx, error) {
-	conn, err := cluster.getConn()
+	conn, err := cluster.GetConn()
 	if err != nil {
 		return nil, err
 	}
 	return conn.Begin()
 }
 
-type ClusterDriver struct {
-	cluster *Cluster
+// Open is a stub implementation to satisfy database/sql/driver interface. It does not do anything apart from returning its parent type.
+// NOTE: While the name argument does not do anything at this point, this may change in the future to allow the setting of e.g., timeout options
+func (cluster Cluster) Open(name string) (driver.Conn, error) {
+	return cluster, nil
 }
 
-func NewDriver(upstreamDriver driver.Driver) ClusterDriver {
-	return ClusterDriver{&Cluster{map[string]*Node{}, upstreamDriver}}
-}
-
-func (d *ClusterDriver) AddNode(name, dataSourceName string) {
-	d.cluster.AddNode(name, dataSourceName)
-}
-
-func (d ClusterDriver) Open(name string) (driver.Conn, error) {
-	return d.cluster, nil
+// NewDriver returns an initialized Cluster driver, using upstreamDriver as backend
+func NewDriver(upstreamDriver driver.Driver) Cluster {
+	return Cluster{map[string]*Node{}, upstreamDriver}
 }
