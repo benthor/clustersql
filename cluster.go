e41a5a88af18eaf77d8c32c728ced5a00ffcbc4f
Thorben Krüger <tkr@ecix.net>
2014-08-11 17:10:35 +0200

    travis shenanigans and documentation style fix

--- cluster.go
+++ cluster.go
@@ -23,8 +23,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-// Go Clustering SQL Driver - A clustering, implementation-agnostic
-// "meta"-driver for any backend implementing "database/sql/driver".
+// Package clustersql is an SQL "meta"-Driver - A clustering, implementation-
+// agnostic wrapper for any backend implementing "database/sql/driver".
 //
 // It does (latency-based) load-balancing and error-recovery over all registered
 // nodes.
@@ -83,8 +83,8 @@ type ClusterError struct {
 	Message string
 }
 
-func (me ClusterError) Error() string {
-	return me.Message
+func (ce ClusterError) Error() string {
+	return ce.Message
 }
 
 // Cluster is a type which implements "database/sql/driver"
